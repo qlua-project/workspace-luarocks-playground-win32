01d1c287d9729f03a370e97c77f13621792bac9c
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-15 21:59:35 +0300

    luarocks installed initial

---
+++ luarocks/config-5.4.lua
@@ -0,0 +1,14 @@
+rocks_trees = {
+    home..[[/luarocks]],
+    { name = [[user]],
+         root    = home..[[/luarocks]],
+    },
+    { name = [[system]],
+         root    = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks\systree]],
+    },
+}
+variables = {
+    MSVCRT = 'VCRUNTIME140',
+}
+verbose = false   -- set to 'true' to enable verbose output
+fs_use_modules = false   -- prevent LuaRocks itself from using installed modules and blocking their files from removal 
---
+++ luarocks/lua/luarocks/core/hardcoded.lua
@@ -0,0 +1,14 @@
+return {
+   LUA_DIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua]],
+   LUA_INCDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\include]],
+   LUA_LIBDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\lib]],
+   LUA_BINDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\bin]],
+   LUA_VERSION=[[5.4]],
+   LUA=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\bin\lua.exe]],
+   SYSTEM = [[windows]],
+   PROCESSOR = [[x86_64]],
+   PREFIX = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks]],
+   SYSCONFDIR = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks]],
+   WIN_TOOLS = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks/tools]],
+   FORCE_CONFIG = true,
+}
