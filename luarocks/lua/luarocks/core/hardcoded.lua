99af6a21cfa7caa55c13523a1bb228944349f5a6
ivansoft <32499694+ivansoft@users.noreply.github.com>
2025-03-15 23:34:07 +0300

    paths absolute -> relative core/hardcoded.lua

--- luarocks/lua/luarocks/core/hardcoded.lua
+++ luarocks/lua/luarocks/core/hardcoded.lua
@@ -1,14 +1,23 @@
+local dir = require("luarocks.core.dir")
+
+local function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*\\)")
+end
+local harcoded_path = script_path()
+local workspace_path = dir.normalize(harcoded_path .. [[..\..\..\..]])
+
 return {
-   LUA_DIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua]],
-   LUA_INCDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\include]],
-   LUA_LIBDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\lib]],
-   LUA_BINDIR=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\bin]],
+   LUA_DIR=workspace_path..[[\externals\lua]],
+   LUA_INCDIR=workspace_path..[[\externals\lua\include]],
+   LUA_LIBDIR=workspace_path..[[\externals\lua\lib]],
+   LUA_BINDIR=workspace_path..[[\externals\lua\bin]],
    LUA_VERSION=[[5.4]],
-   LUA=[[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\externals\lua\bin\lua.exe]],
+   LUA=workspace_path..[[\externals\lua\bin\lua.exe]],
    SYSTEM = [[windows]],
    PROCESSOR = [[x86_64]],
-   PREFIX = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks]],
-   SYSCONFDIR = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks]],
-   WIN_TOOLS = [[C:\Users\ivansoft\Development\TEST\qlua-project\workspace_luarocks\luarocks/tools]],
+   PREFIX = workspace_path..[[\luarocks]],
+   SYSCONFDIR = workspace_path..[[\luarocks]],
+   WIN_TOOLS = workspace_path..[[\luarocks/tools]],
    FORCE_CONFIG = true,
 }
